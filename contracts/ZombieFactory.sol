2069ac4444f83211b93fda18db961c48786728f2
Rodrigo Villarreal <noiarek@gmail.com>
2018-08-23 18:52:08 +1200

    Adding more comments to contracts

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.24;
 
 contract ZombieFactory {
 
@@ -13,30 +13,40 @@ contract ZombieFactory {
     }
 
     /**
-    These variables return a function, so to get them we need to
+    These variables (mapping and Zombie[] return a promise, 
+    so to get the values of them we need to
+    
     > instance.zombies(0).then(function(z){ a = z})
     > a[0]
      */
 
     Zombie[] public zombies;
 
+    // id = 1 => address = 0x5a4b3f2cc2d3a4bc6dee3
     mapping (uint => address) public zombieToOwner;
+    
+    // address = 0x5a4b3f2cc2d3a4bc6dee3 => count = 3 Zombies
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
         // push returns the length of zombies
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
+
+        // Emit an event telling to the listeners that a new zombie was created
         emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
+        // Always strings are handled with keccak256 (to compare or whatever you need)
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string _name) public {
+        // require is a validation. If the validation fails, the code doesnt continue
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         randDna = randDna - randDna % 100;
--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -1,7 +1,8 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.24;
 
 import "./ZombieFactory.sol";
 
+// Interface from CryptoKitty. It helps to connect to the contract
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -17,26 +18,32 @@ contract KittyInterface {
   );
 }
 
+// ZombieFeeding inherits ZombieFactory properties
 contract ZombieFeeding is ZombieFactory {
 
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  
+  // Create connection to external contract, using the address and the interface
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
-  // Modify function definition here:
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
+
+    // Investigate this
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    // Add an if statement here
+    if(keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
+    }
     _createZombie("NoName", newDna);
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
+    // Getting last value from getKitty() (ignoring the other values)
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    // And modify function call here:
-    feedAndMultiply(_zombieId, kittyDna);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
 }
